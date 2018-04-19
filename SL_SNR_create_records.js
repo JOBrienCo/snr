bd3c836fee52d92c4035e8aca7bc0321ed7d36c8
WWinters <WWinters@jobrien.com>
2018-04-19 15:54:46 -0400

    Properly adds serial number to custom snr record

--- SL_SNR_create_records.js
+++ SL_SNR_create_records.js
@@ -3,11 +3,11 @@
  * @NScriptType Suitelet
  * @NModuleScope SameAccount
  */
-define(['N/record'],
+define(['N/record', 'N/search'],
 /**
  * @param {record} record
  */
-function(record) {
+function(record, search) {
    
     /**
      * Definition of the Suitelet script trigger point.
@@ -107,10 +107,88 @@ function(record) {
 			title: 'DEBUG',
 			details: recRAId 
 		});
+		
+		if (!parameters.SNserialId) {
+			//Load SNR record if serial ID does not exist (new)
+			recSNR = record.load({
+				type:	parameters.SNRType,
+				id:		parameters.SNR
+			});
+			recNewRA = record.load({
+				type:	record.Type.RETURN_AUTHORIZATION,
+				id:		recRAId
+			});
+			var serialId = getSerialId(recNewRA);
+			recSNR.setValue({
+				fieldId:	'custrecord_snr_sn',
+				value:		serialId
+			});
+			var recSNRId = recSNR.save();
+			log.debug({
+				title: 'recSNRid',
+				details: recSNRId
+			});
+		}
+		
     }
-
     return {
     	onRequest: createReturn
     };
     
+    function getSerialId(record) {
+    	//Grab serial number from first line item of record
+    	var itemId = record.getSublistValue({
+    		sublistId:		'item',
+    		fieldId:		'item',
+    		line:			0
+    	});
+    	log.debug({
+    		title: 'itemId',
+    		details: itemId
+    	});
+    	var inventoryDetailSubrecord = record.getSublistSubrecord({
+    		sublistId:	'item',
+    		fieldId:	'inventorydetail',
+    		line:		0
+    	});
+    	var inventoryNumber = inventoryDetailSubrecord.getSublistValue({
+    		sublistId:	'inventoryassignment',
+    		fieldId:	'receiptinventorynumber',	//This returns the actual serial number
+    		//fieldId:	'inventorydetail',			//This returns the internal ID of the S/N
+    		line:		0
+    	});	
+    	log.debug({
+    		title: 'inventoryNumber',
+    		details: inventoryNumber
+    	});
+    	var inventoryNumberInternalId = getInventoryNumberInternalId(inventoryNumber,itemId)
+    	return inventoryNumberInternalId;
+    }
+    
+    function getInventoryNumberInternalId(inventoryNumber,itemId) {
+    	var inventoryNumberSearchObj = search.create({
+    		type:		'inventorynumber',
+    		filters:	
+    			[
+    			      ["inventorynumber","is",inventoryNumber], 
+    			      "AND", 
+    			      ["item","anyof",itemId]
+    			   ],
+    			   columns:
+    			   [
+    			      "internalid"
+    			   ]
+    	});
+    	inventoryNumberSearchObj.run().each(function(result){
+    		inventoryNumberId = result.getValue({name: 'internalid'});
+    		return true;
+    	});
+    	log.debug({
+    		title: 'inventoryNumberId',
+    		details: inventoryNumberId
+    	});
+    	return inventoryNumberId;
+    }
 });
+
+
